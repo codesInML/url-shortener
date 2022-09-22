72615b27e50b21aee9222ce03814cbfa046c4d69
Ifeoluwa Olubo <ifeoluwaolubo@gmail.com>
2022-09-22 09:25:15 +0100

    added cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const { connectDB } = require("./db");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(cors());
 app.use(express.json({ extended: false }));
 
 app.use("/", require("./url"));
